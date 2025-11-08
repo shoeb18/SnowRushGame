16620141da69d4b82e3c0c16958d78e5065ee37b
shoeb khan <shoebkhan0197@gmail.com>
2025-11-08 11:39:42 +0530

    Add particle effects for crash and finish line events

--- Assets/Scripts/CrashDetector.cs
+++ Assets/Scripts/CrashDetector.cs
@@ -3,10 +3,13 @@ using UnityEngine.SceneManagement;
 
 public class CrashDetector : MonoBehaviour
 {
+    [SerializeField] ParticleSystem crashFX;
+
     void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Ground"))
         {
+            crashFX.Play();
             Invoke("ReloadScene", 1f);
         }
     }
--- Assets/Scripts/FinishLine.cs
+++ Assets/Scripts/FinishLine.cs
@@ -3,10 +3,13 @@ using UnityEngine.SceneManagement;
 
 public class FinishLine : MonoBehaviour
 {
+    [SerializeField] ParticleSystem finishFX;
+
     void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
+            finishFX.Play();
             Invoke("ReloadScene", 2f);
         }
     }
